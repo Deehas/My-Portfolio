270b4ff587f14e9b57d3f1a6ac743323b5972ec4
Deehas <sailass0221@gmail.com>
2021-08-24 21:05:47 +0100

    change button onclick

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Side Navigation Menu JS Code
+let body = document.querySelector("body");
 let menuBtn = document.querySelector(".menu-btn");
 let cancelBtn = document.querySelector(".cancel-btn");
 let navBar = document.querySelector(".navbar");
@@ -8,20 +9,38 @@ menuBtn.onclick = function(){
     menuBtn.style.opacity = "0";
     menuBtn.style.pointerEvents = "none";
     navBar.classList.add("active");
+    body.style.overflow = "hidden";
+    scrollBtn.style.pointerEvents = "none";
 }
 cancelBtn.onclick = function(){
     menuBtn.style.opacity = "1";
     menuBtn.style.pointerEvents = "auto";
     navBar.classList.remove("active");
+    body.style.overflow = "auto";
+    scrollBtn.style.pointerEvents = "auto";
 }
 
 // Sticky Navigation Menu JS Code
 let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+console.log(scrollBtn);
 let val;
 window.onscroll = function() {
     if(document.documentElement.scrollTop > 20){
         nav.classList.add("sticky");
+        scrollBtn.style.display = "block";
     }else{
         nav.classList.remove("sticky");
+        scrollBtn.style.display = "none";
     }    
+}
+
+// Side Navigation Bar Close While We Click On Navigation Links
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click" , function() {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+  });
 }
